80529c4fdcc9464c2883311fb0acabbe94fa899a
Juan Barillas <juanbarillas@ufm.edu>
2021-01-25 23:09:33 -0600

    Updated problem 4 extra points, already finished.

--- clase4/Problema4.py
+++ clase4/Problema4.py
@@ -1,1 +1,16 @@
-print("Resolución Problema 4")
+'''
+El código fue obtenido de stackoverflow https://stackoverflow.com/questions/2068372/fastest-way-to-list-all-primes-below-n 
+investigando en la página  https://www.quora.com/How-do-you-write-a-relatively-simple-yet-fast-Python-program-for-generating-prime-numbers
+'''
+
+def get_primes(n):
+    numbers = set(range(n, 1, -1))
+    primes = []
+    while numbers:
+        p = numbers.pop()
+        primes.append(p)
+        numbers.difference_update(set(range(p*2, n+1, p)))
+    return primes
+
+get_primes(100000)
+print(get_primes(100000))
