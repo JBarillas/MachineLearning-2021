3919d0004bc4e71fdef463f33c9a88f2cbbe02f5
Juan Barillas <juanbarillas@ufm.edu>
2021-01-26 14:42:00 -0600

    Problem 2 implemented. Must change to focus on cache.

--- clase4/Problema2.py
+++ clase4/Problema2.py
@@ -1,1 +1,11 @@
-print("Resolución Problema 2")
+#Calcular la función utilizando recurrencia, n y m son naturales (incluye 0)
+
+def equation(n,m):
+    if m == n:
+        return 1
+    if m == 0:
+        return 1
+    if (n >= m) and (m > 0):
+        return equation(n-1,m) + equation(n-1, m-1)
+#Calcule (50,35) y (100,85)
+print(equation(50,35))
