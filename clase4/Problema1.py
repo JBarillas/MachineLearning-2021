3f455f8813ccc87232215a22b5b3d57519da72e3
Juan Barillas <juanbarillas@ufm.edu>
2021-01-25 21:51:11 -0600

    Updated problem 1,  already solved.

--- clase4/Problema1.py
+++ clase4/Problema1.py
@@ -1,1 +1,11 @@
-print("Resolución Problema 1")
+#Imprima un triángulo invertido dependiendo de su altura
+
+upside_triangle = lambda n: '\n'.join([
+    f"{' ' * (n - i - 1)}{'*' * (2 * i + 1)}"
+    for i in range(n - 1, -1, -1)
+])
+
+#Imprimir las figuras con alturas 4, 5 y 6
+print(upside_triangle(4)+'\n')
+print(upside_triangle(5)+'\n')
+print(upside_triangle(6)+'\n')
