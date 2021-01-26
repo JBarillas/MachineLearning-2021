b3b07d0765566226d8e867c279597490dc7b6145
Juan Barillas <juanbarillas@ufm.edu>
2021-01-25 22:50:27 -0600

    Updated problem 3, already finished.

--- clase4/Problema3.py
+++ clase4/Problema3.py
@@ -1,1 +1,12 @@
-print("Resolución Problema 3")
+#Imprimir un rumbo dependiendo de su altura
+import math
+
+diamond = lambda n: '\n'.join([
+    f"{' ' * (abs(math.ceil(n/2) - 1 - i))}{'* ' * (math.ceil(n/2) -(abs(math.ceil(n/2) - 1 - i)))}"
+    for i in range(n)
+])
+
+#Imprimir figuras con las alturas 7,9,11
+print(diamond(7)+"\n")
+print(diamond(9)+"\n")
+print(diamond(11)+"\n")
