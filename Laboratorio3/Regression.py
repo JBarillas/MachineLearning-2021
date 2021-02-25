f52cfd099fca87b8a376169b79a65371372dbdd2
Juan Barillas <juanbarillas@ufm.edu>
2021-02-25 15:34:42 -0600

    Added Lab3

---
+++ Laboratorio3/Regression.py
@@ -0,0 +1,33 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import math
+
+# data ---------------------------
+df = pd.read_csv('satgpa.csv')
+
+# equations ----------------------
+def linear_cost(X, y, theta):
+    h = X @ theta
+    return ((h - y) ** 2).sum() / (2*len(X))
+
+def linear_cost_gradient(X, y, theta):
+    h = X @ theta
+    return ((h - y).T @ X).T / (2*len(X))
+
+def gradient_descent(
+    X, y, theta_0, 
+    cost_function, cost_function_gradient,
+    learning_rate=0.01, 
+    threshold=0.001,
+    max_iter=1000
+):
+    theta = theta_0
+    iteration = 0
+    history = []
+    while np.linalg.norm(cost_function_gradient(X, y, theta)) > threshold and iteration < max_iter:
+        iteration += 1
+        theta -= learning_rate * cost_function_gradient(X, y, theta)
+        history.append(theta - learning_rate * cost_function_gradient(X, y, theta))
+
+    return theta, history
